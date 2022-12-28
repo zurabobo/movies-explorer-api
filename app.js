f53376375ea8b0f8624bc2b2d702d87885ef125c
Zura <zuramariwedding@gmail.com>
2022-12-28 14:25:12 +0400

    deployment on render.com

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const app = express();
 const options = {
   origin: [
     'http://localhost:8080',
-    'https://moviesexplorer-api.herokuapp.com',
+    'https://movies-explorer-api-yv7u.onrender.com',
     'https://movies-explorer-frontend.netlify.app'
     // 'https://movies-explorer.zb.nomoredomains.rocks',
     // 'https://api.movies-explorer.zb.nomoredomains.rocks',
